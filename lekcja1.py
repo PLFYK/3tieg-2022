cb87125d75cb5674a0b7374f5b4e06b486631c59
PLFYK <97887025+PLFYK@users.noreply.github.com>
2022-03-18 08:15:35 +0100

    2022-03-18

--- lekcja1.py
+++ lekcja1.py
@@ -3,29 +3,40 @@ import random
 import waz
 import jablko
 
+#zmienne globalne 
+kolorWaz1=(255,255,0)
+kolorWaz2=(0,255,255)
+obiektWaz1=waz.Waz()
+obiektWaz2=waz.Waz()
+def zmianaKolorWaz1(kolor):
+    obiektWaz1.ustawKolor(kolor)
+def zmianaKolorWaz2(kolor):
+    obiektWaz2.ustawKolor(kolor)
+iloscJablek=3
+def zmianaIloscJablek(ilosc):
+    iloscJablek=ilosc
+
 def main():
     pygame.init()
     OknoGry=pygame.display.set_mode((440,440),0,32)
     pygame.display.set_caption("3tieg")
-    run=True
-    iloscJablek=3
+    run=True 
     #wywołanie klasy waz
-    obiektWaz1=waz.Waz()
-    obiektWaz2=waz.Waz()
-    #tworzeniu kilku jablek
-    obiektJablok=[]
+    #tworzenie kilku jablek
+    obiektJablko=[]
     for nrJablko in range(0,iloscJablek):
-        obiektJablok.append(jablko.Jablko())    
+        obiektJablko.append(jablko.Jablko())
+    
     #losowanie pozycji jabłka
     appleX=random.randint(0,21)*20+10
     appleY=random.randint(0,21)*20+10
     
     while(run):
         OknoGry.fill((0,0,0))
-
-        for obiektApple in obiektJablok[::]:
+        
+        for obiektApple in obiektJablko[::]:
             obiektApple.rysujJablko(OknoGry)
-        #obiektJablok.rysujJablko(OknoGry)
+        #obiektJablko.rysujJablko(OknoGry)
         pygame.time.delay(200)
         #obsługa ruchu weża obiektu obiket waz
         for zdarzenie in pygame.event.get():
@@ -40,7 +51,7 @@ def main():
                     obiektWaz1.setKierunek((0,-1))
                 elif zdarzenie.key==pygame.K_DOWN:
                     obiektWaz1.setKierunek((0,1))
-                if zdarzenie.key==pygame.K_a:
+                elif zdarzenie.key==pygame.K_a:
                     obiektWaz2.setKierunek((-1,0))
                 elif zdarzenie.key==pygame.K_d:
                     obiektWaz2.setKierunek((1,0))
@@ -57,22 +68,27 @@ def main():
         #tworzenie jablka za pomoca kola
         #pygame.draw.circle(OknoGry,(128,0,0),(appleX,appleY),10)
         
-        #spraw dzenie czy waz zjada jablkod
+        #sprawdzenie czy waz zjada jablko
         poz1=obiektWaz1.getPosition()
         poz2=obiektWaz2.getPosition()
-        for obiektApple in obiektJablok[::]:
+        for obiektApple in obiektJablko[::]:
             pozJablko=obiektApple.getPozycja()
             if (poz1[1]+10==pozJablko[1] and poz1[0]+10==pozJablko[0]):
                 obiektWaz1.zjadanie()
+                obiektApple.losujPozycje()
             if (poz2[1]+10==pozJablko[1] and poz2[0]+10==pozJablko[0]):
                 obiektWaz2.zjadanie()
             #wylosowanie nowej pozycji jablka
                 obiektApple.losujPozycje()
-            
+            #appleX=random.randint(0,21)*20+10
+            #appleY=random.randint(0,21)*20+10
+            #pygame.draw.circle(OknoGry,(128,128,128),(appleX,appleY),10)
+        obiektWaz1.czyKtosMnieUgryzl(poz2)
+        obiektWaz2.czyKtosMnieUgryzl(poz1)
         #wypisanie punktow na ekran
         czcionka=pygame.font.SysFont('comicsans',20)
-        tekst=czcionka.render("Gracz1 punkty: {0}".format(obiektWaz1.punkty),1,(0,255,0))
-        tekst2=czcionka.render("Gracz2 punkty: {0}".format(obiektWaz2.punkty),1,(0,255,0))
+        tekst=czcionka.render("Punkty gracz 1: {0}".format(obiektWaz1.punkty),1,(0,255,0))
+        tekst2=czcionka.render("Punkty gracz 2: {0}".format(obiektWaz2.punkty),1,(0,255,0))
         OknoGry.blit(tekst, (10,10))
         OknoGry.blit(tekst2, (10,30))
         
@@ -104,7 +120,8 @@ def main():
         #góra ekranu
         if glowa2[1]<0:
             obiektWaz2.setPosition(glowa2[0],420)
-          
+
+
         pygame.display.update()
 
-main()
+#main()
---
+++ start.py
@@ -0,0 +1,25 @@
+import pygame
+import pygame_menu
+import lekcja1
+
+def wlaczGre():
+    lekcja1.main()
+def zmienKolorWaz1(value):
+    lekcja1.zmianaKolorWaz1(value)
+def zmienKolorWaz2(value):
+    lekcja1.zmianaKolorWaz2(value)
+def zmienJablka(wartosc,value):
+    lekcja1.iloscJablek=value
+def main():
+    pygame.init()
+    oknoMenu=pygame.display.set_mode((500,500))
+    pygame.display.set_caption("Wąż")
+    menu=pygame_menu.Menu("Gra Snake 3TIEG", 500,500,theme=pygame_menu.themes.THEME_DARK)
+    menu.add.button("Włącz grę",wlaczGre,background_color=(0,0,255))
+    menu.add.color_input('Kolor Wąż 1:','rgb',default=(100,100,255),onreturn=zmienKolorWaz1)
+    menu.add.color_input('Kolor Wąż 1:','rgb',default=(100,100,255),onreturn=zmienKolorWaz2)
+    #menu.add.button('Zapisz kolor wąż 1')
+    menu.add.selector("Wybierz ilosc jablek",[('jedno',1),('dwa',2),('trzy',3),('cztery',4),('piec',5)],onchange=zmienJablka)
+    menu.mainloop(oknoMenu)
+
+main()
